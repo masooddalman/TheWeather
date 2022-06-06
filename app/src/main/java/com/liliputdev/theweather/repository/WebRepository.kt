fb8e9e7c9cbb61688286c1b0067b47fbde4ef3c7
Masood Dalman <m.dalman68@gmail.com>
2022-06-06 15:15:34 +0300

    change key in WebRepository.kt

--- app/src/main/java/com/liliputdev/theweather/repository/WebRepository.kt
+++ app/src/main/java/com/liliputdev/theweather/repository/WebRepository.kt
@@ -4,7 +4,7 @@ import com.liliputdev.theweather.repository.retrofit.RetrofitService
 
 class WebRepository constructor(private val retrofitService: RetrofitService){
     private val host="community-open-weather-map.p.rapidapi.com"
-    private val key="14e24a7333mshe360883d2b5b6eap1152cdjsn0ff0bdf838f5"
+    private val key="7f5036a166mshef6a10ee3c1b431p1aa45cjsna815b8fecefd"
 
     fun searchByCity(city:String) = retrofitService.searchByCity(city,host,key)
 
